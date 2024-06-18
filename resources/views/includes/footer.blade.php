55e76ac96c66da254468874eff370252960ad794
Sergey <150217369+MoonlightMediaGroup@users.noreply.github.com>
2024-06-19 09:14:02 +1000

    Test

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
     <div class="col-md-4 d-flex align-items-center">
-      <p>&copy; 2023 Moonlight-Media, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+      <p>&copy; 2024 Moonlight-Media, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
     </div>
     @if (Route::currentRouteName() != 'welcome')
       <ul class="nav justify-content-end list-unstyled d-flex">
